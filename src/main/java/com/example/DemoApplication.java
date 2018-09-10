4e9dc6114494fdd9abe3c93d04c417ea8f65d2ee
Hendi Santika <hendisantika@gmail.com>
2018-09-10 08:30:11 +0700

    #4 - Upgraded into Spring Boot 1.5.15.RELEASE version

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,14 +1,12 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class DemoApplication {
 
 	@RequestMapping("/")
